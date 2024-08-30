1610e7f373f9326b3e3fe4dbe8f06705062defbd
aldojasb <aldosaltao@gmail.com>
2024-08-30 15:59:36 -0700

    refactoring logging and init.py

--- database_generator/src/database_generator/__init__.py
+++ database_generator/src/database_generator/__init__.py
@@ -1,9 +1,9 @@
-# __init__.py
+# # __init__.py
 
-from .initializer import initialize_logging_configuration
+# from .initializer import initialize_logging_configuration
 
-# Initialize the package
-timestamp_for_this_experiment = initialize_logging_configuration()
+# # Initialize the package
+# timestamp_for_this_experiment = initialize_logging_configuration()
 
-# Make timestamp_for_this_experiment available package-wide
-__all__ = ['timestamp_for_this_experiment']
+# # Make timestamp_for_this_experiment available package-wide
+# __all__ = ['timestamp_for_this_experiment']
--- database_generator/src/database_generator/logging_configuration.py
+++ database_generator/src/database_generator/logging_configuration.py
@@ -1,5 +1,7 @@
-import logging
+import json
 import os
+import logging
+from database_generator.helpers import get_config_path
 
 def setup_logging(log_file_path=None, log_level=logging.INFO):
     """
@@ -31,3 +33,38 @@ def setup_logging(log_file_path=None, log_level=logging.INFO):
             format=log_format,
             datefmt=date_format
         )
+
+
+def initialize_logging_configuration():
+    ##### Step 0: Get the path to the configuration file
+
+    path_for_the_json_file = get_config_path()
+
+    ##### Step 1: Read variables from the JSON file
+
+    with open(path_for_the_json_file, 'r') as file:
+        params = json.load(file)
+
+    training_parameters = params["parameters_to_train_the_model"]
+    variable_of_interest = training_parameters["variable_of_interest"]
+
+    parameters_to_save_the_outcomes = params["parameters_to_save_the_outcomes"]
+    path_to_save_the_outcomes = parameters_to_save_the_outcomes["path_to_save_the_outcomes"]
+
+    ##### Step 2: Construct the dynamic log file path
+    log_file_path = os.path.join(
+        path_to_save_the_outcomes,
+        "tmp",
+        "log_records",
+        variable_of_interest,
+        "anomaly_detection_app.log"
+    )
+
+    # Ensure the log directory exists
+    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+
+    # Step 3: Setup logging with the dynamic path
+    setup_logging(log_file_path=log_file_path)
+    
+    logging.getLogger(__name__).info("Logging is set up correctly.")
+
