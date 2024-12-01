8b6d473246679ac31017a08ca46587eeb379fffd
Aldo Saltao Barros <aldosaltao@gmail.com>
2024-12-01 00:03:58 +0000

    refactoring PATH and make it lightweighter

--- cookiecutter_template_v1/hooks/post_gen_project.sh
+++ cookiecutter_template_v1/hooks/post_gen_project.sh
@@ -4,8 +4,12 @@ set -e  # Exit immediately if a command exits with a non-zero status
 set -x  # Print commands and their arguments as they are executed
 
 # Define the path to the templates and the destination directory
-TEMPLATES_PATH="/home/aldo/Repositories/general_projects/cookiecutter_template_v1/_templates"
+TEMPLATES_PATH="/workspace/general_projects/cookiecutter_template_v1/_templates"
 DEST_DIR="src/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}"
+
+# Debug output
+echo "TEMPLATES_PATH is $TEMPLATES_PATH"
+echo "DEST_DIR is $DEST_DIR"
 
 # Create the src directory
 mkdir -p "$DEST_DIR"
@@ -16,7 +20,6 @@ cp "$TEMPLATES_PATH/__init__.py" "$DEST_DIR/"
 cp "$TEMPLATES_PATH/evaluate.py" "$DEST_DIR/"
 cp "$TEMPLATES_PATH/get_data.py" "$DEST_DIR/"
 cp "$TEMPLATES_PATH/helpers.py" "$DEST_DIR/"
-cp "$TEMPLATES_PATH/initializer.py" "$DEST_DIR/"
 cp "$TEMPLATES_PATH/logging_configuration.py" "$DEST_DIR/"
 cp "$TEMPLATES_PATH/main.py" "$DEST_DIR/"
 cp "$TEMPLATES_PATH/predict.py" "$DEST_DIR/"
