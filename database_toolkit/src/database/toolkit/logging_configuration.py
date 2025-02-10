874c95a145ee417b3ee24ca12f72b55e0b3f33d6
asaltao <asaltaobarros@gmail.com>
2025-02-10 23:26:49 +0000

    updating package name

---
+++ database_toolkit/src/database/toolkit/logging_configuration.py
@@ -0,0 +1,67 @@
+import logging
+import os
+
+def setup_logging(log_file_path=None, log_level=logging.INFO):
+    """
+    Setup logging configuration.
+    
+    Args:
+        log_file_path (str): Path to the log file. If None, logs will be displayed on the console.
+        log_level (int): Logging level. Default is logging.INFO.
+    """
+    # Updated format to include the logger's name
+    log_format = "[%(name)s] %(asctime)s - %(levelname)s: %(message)s"
+    date_format = "%Y-%m-%d %H:%M:%S"
+    
+    if log_file_path:
+        # Ensure the log directory exists
+        os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+        
+        logging.basicConfig(
+            level=log_level,
+            format=log_format,
+            datefmt=date_format,
+            handlers=[
+                logging.FileHandler(log_file_path),
+                logging.StreamHandler()
+            ]
+        )
+    else:
+        logging.basicConfig(
+            level=log_level,
+            format=log_format,
+            datefmt=date_format
+        )
+
+def setup_logging_for_this_script(log_level=logging.DEBUG) -> None:
+    """
+    Set up logging for the current script, ensuring the log directory and file exist.
+
+    Args:
+        log_level (int): Logging level. Default is logging.INFO.
+    """
+    # Retrieve the base path for saving logs from the environment variable
+    path_to_save_the_outcomes = os.getenv('PATH_TO_SAVE_THE_OUTCOMES')
+    
+    # Check if the environment variable is defined# Get the logger for this module
+# logger = logging.getLogger(__name__)
+
+    if not path_to_save_the_outcomes:
+        raise EnvironmentError("Environment variable 'PATH_TO_SAVE_THE_OUTCOMES' is not defined. "
+                               "Please set it in your environment or .env file.")
+
+    # Validate that the path exists and is writable
+    if not os.path.exists(path_to_save_the_outcomes):
+        raise FileNotFoundError(f"The specified path '{path_to_save_the_outcomes}' does not exist. "
+                                "Please create the directory or specify a valid path.")
+    if not os.access(path_to_save_the_outcomes, os.W_OK):
+        raise PermissionError(f"The specified path '{path_to_save_the_outcomes}' is not writable. "
+                              "Please check the directory permissions.")
+
+    # Setup logging directory and file
+    log_dir_path = os.path.join(path_to_save_the_outcomes, 'tmp', 'log_records')
+    os.makedirs(log_dir_path, exist_ok=True)  # Ensure that the log directory exists
+    log_file_path = os.path.join(log_dir_path, "logs.log")  # Create the log file path
+
+    # Call the setup_logging function with the constructed log file path
+    setup_logging(log_file_path=log_file_path, log_level=log_level)
